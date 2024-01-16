823c8a20335627fc60c97988cf2188a804dc1abc
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2024-01-16 14:53:17 +0530

    Added the server

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import {FaSearch} from 'react-icons/fa';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+
+export default function Header() {
+
+  const {currentUser} = useSelector((state) => state.user);
+  return (
+    <header className='bg-slate-300 shadow-md'>
+        <div className='flex justify-between items-center mx-auto max-w-6xl p-3'>
+            <Link to='/'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-500'>Real</span>
+                    <span className='text-slate-600'>Estate</span>
+                    <span className='text-slate-800'>Hub</span>
+                </h1>
+            </Link>
+            <form className='bg-slate-100 p-2 rounded-xl flex items-center'>
+                <input type="text" placeholder='search..' className='bg-transparent
+                focus:outline-none w-24 sm:w-64' />
+                <FaSearch className='text-slate-600' />
+            </form>
+            <ul className='flex gap-4 '>
+                <Link to='/'>
+                    <li className='hidden sm:inline text-slate-800 hover:underline'>Home</li>
+                </Link>
+                <Link to='/About'>
+                    <li className='hidden sm:inline text-slate-800 hover:underline'>About</li>
+                </Link>
+                <Link to='/profile'>
+                {currentUser ? (
+                <img
+                    className='rounded-full h-7 w-7 object-cover'
+                    src={currentUser.avatar}
+                    alt='profile'
+                ></img>
+                ) : (
+                <li className=' text-slate-700 hover:underline'> Sign in</li>
+                )}
+            </Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
