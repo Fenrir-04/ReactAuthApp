4e52cfdbc2aebb8c0beb5142eb49b9fc214fe9ad
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2024-01-16 15:37:13 +0530

    Connected MongoDB and created a middleware for error handling

--- api/index.js
+++ api/index.js
@@ -1,10 +1,44 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+dotenv.config();
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log('Database connected successfully!'); 
+})
+.catch(err => {
+  console.log('Could not connect to database!', err);
+});
+
+app.use(express.json());
+
+
+// middleware to handle errors in baackend
+app.use((err,req,res,next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server issue!';
+  return res.status(statusCode)
+  .json(
+      {
+          success: false,
+          statusCode,
+          message,
+      }
+  );
+});
+
+
 
 
 
